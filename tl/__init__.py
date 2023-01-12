2208d53b9c55c4294efd7f51cbe82cda675e0a66
CY <aubot@hotmail.com>
2023-01-12 17:35:09 +0800

    12

--- tl/__init__.py
+++ tl/__init__.py
@@ -1,4 +1,4 @@
-from ._deg import deseq,deg_siglabel,deg_filter,
+from ._deg import deseq,deg_siglabel,deg_filter
 from ._rna import geneIDconverter,unique_exprs,count2tpm,get_TCGA_mRNA
 from ._frame import select,subset
 from .IO import export
