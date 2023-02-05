7d6f0c3f8fcbfe934c06f2412310b334d04ec784
生信探索 <aubot@hotmail.com>
2023-02-05 17:26:40 +0800

    add flatten

--- tl/utils.py
+++ tl/utils.py
@@ -1,9 +1,11 @@
 import itertools
 
-def flatten(nest_list):
-    nl = nest_list.copy()
-    import itertools
-    for x,y in enumerate(nl):
-        if not isinstance(y, list):
-            nl[x] = [y]
-    return list(itertools.chain.from_iterable(nl))
+# def flatten(nest_list):
+#     nl = nest_list.copy()
+#     import itertools
+#     for x,y in enumerate(nl):
+#         if not isinstance(y, list):
+#             nl[x] = [y]
+#     return list(itertools.chain.from_iterable(nl))
+
+flatten = lambda nest_list: sum(([x] if not isinstance(x, list) else flatten(x) for x in nest_list), [])
